f68bca25c037b4d492c3125cd1f5d62393a8d39e
Robby Sarvis <rjs.app.dev@gmail.com>
2023-06-23 14:07:30 -0500

    Initial Commit

---
+++ ws-chat-client/ws_chat_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ws_chat_clientApp.swift
+//  ws-chat-client
+//
+//  Created by Robert J. Sarvis Jr on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct ws_chat_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
